df6d5b1abb2646bf9f71337565c5b5763137160d
llfssaa <liulia.1758@gmail.com>
2022-05-02 23:17:06 +0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+        <p>Счетчик: {count}</p>
+        <button onClick={()=>setCount(count+1)}>Увеличить счетчик</button>
+        <button onClick={()=>setCount(count-1)} >Уменьшить счетчик</button>
+        <button onClick={()=>setCount(0)}>Сбросить</button>
+
     </div>
   );
 }
